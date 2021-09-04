54b1eb89fe4c00305acf5a81b3b3bb530e979ffd
AKhudoshin <83674906+AKhudoshin@users.noreply.github.com>
2021-09-04 13:59:11 +0300

    Add files via upload

--- measurements.c
+++ measurements.c
@@ -26,11 +26,11 @@
       printf("Error opening file\n");
       return -2;
     }
-
-    struct meas *m;  
+ 
     static int meas_count = 0; 
     static int meas_error = 0;
     static int meas_size = 100;
+    struct meas *m; 
 
     m = malloc(sizeof(struct meas) * meas_size);
 
@@ -43,21 +43,35 @@
                   &m->hour,
                   &m->minute,
                   &m->temp);
-      if (scanfret == 6) { 
-        meas_count++;
+      if (scanfret == 6) {
+        m[meas_count].year = m->year;
+        m[meas_count].month = m->month;
+        m[meas_count].day = m->day;
+        m[meas_count].hour = m->hour;
+        m[meas_count].minute = m->minute;
+        m[meas_count].temp = m->temp;
+        meas_count++;  
       } else meas_error++;
       if (meas_count + 1 == meas_size) {
         meas_size *= 2;
         m = realloc(m, sizeof(struct meas) * meas_size);
       }
-      
     }
 
     fclose(f);
 
-    printf("number of elements scanned successfully %d\nerror numbers %d\n", meas_count, meas_error);
     for(int i=0; i <= meas_count; i++) {
-      printf("%d %d %d %d %d %d in a row %d\n", m[i].year, m[i].month, m[i].day, m[i].hour, m[i].minute, m[i].temp, i);
-    }
+        printf("%d %d %d %d %d %d in a row %d\n", 
+        m[i].year, 
+        m[i].month, 
+        m[i].day, 
+        m[i].hour, 
+        m[i].minute, 
+        m[i].temp, 
+        i);
+      } 
+
+    printf("number of elements scanned successfully %d\nerror numbers %d\n", 
+      meas_count, meas_error);
     return 0;
   }
